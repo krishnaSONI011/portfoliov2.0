93f54e2b8198ee24f3f47df053ed8256eabfe268
krishna soni <soni01krishna@gmail.com>
2023-05-07 03:19:15 +0530

    giving scroll bar animation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 export default function Navbar() {
+  let[top,setTop] =useState(0)
+  let scroll 
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+    scroll =window.scrollY
+   
+  })
+  let handleScroll=()=>{
+        let new_scroll =window.scrollY
+        if(new_scroll > scroll){
+            setTop(-70)
+            scroll =window.scrollY
+        }
+        else{
+          setTop(0)
+          scroll =window.scrollY
+
+        }
+  }
   return (
-    <div className='Nav'>
+    <div className='Nav' style={{
+      top:`${top}px`
+    }}>
     <div className='flex justify-cotant-between align-item-center'>
 
       <div className="barnd-name"><h1 className='title'><span className='purple-color size'>&lt;</span>KrishnA<span className='purple-color size'>/&gt;</span></h1></div>
